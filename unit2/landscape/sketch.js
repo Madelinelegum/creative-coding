e19eff60b3cfa7f8c9724bd01b24b8988e43cda6
Madelinelegum <maddierlegum@gmail.com>
2024-10-12 15:27:52 -0400

    update
    update

--- unit2/landscape/sketch.js
+++ unit2/landscape/sketch.js
@@ -7,11 +7,13 @@ function setup() {
 
   // create the canvas
   createCanvas(800, 800);
-  
+
 }
 
 function draw() {
   background(220);
+
+  
 
   // render the image
   image(img,10,10,100,100);
