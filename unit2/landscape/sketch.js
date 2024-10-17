7459287b333776c3daf28864b78071c95cd235ea
Madelinelegum <maddierlegum@gmail.com>
2024-10-16 22:17:12 -0400

    mmm
    ,,

--- unit2/landscape/sketch.js
+++ unit2/landscape/sketch.js
@@ -1,9 +1,5 @@
-function preload(){
-  // load the image from a file
-	img = loadImage();
-}
-
 function setup() {
+
 
   // create the canvas
   createCanvas(800, 800);
@@ -11,10 +7,22 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(133, 194, 230); // Background color // used the link for the light blue color (line 9)- https://editor.p5js.org/Prabashi/sketches/pOiiM-FJC 
+  noStroke();
+  fill (34,139,34); // dark green color for first hill
 
-  
+// move left to right across the canvas
+beginShape();
+  for (let x = 0;x <= width; x+=1){
 
-  // render the image
-  image(img,10,10,100,100);
+    let y = noise(x * 0.005) * height/ 2 + height/2;
+    vertex(x,y);
+    
+  }
+    vertex(width,height);
+    vertex(0,height);
+    endShape(CLOSE); // completes shape and fills in the hill with green
 }
+
+
+
