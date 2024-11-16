7e1b5881f348d997c9c750a05d895c63e9875299
Madelinelegum <maddierlegum@gmail.com>
2024-11-15 20:03:12 -0500

    up

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,21 @@
+let poem = '';
+
+let grammar = tracery.createGrammar({
+"origin": ["A HOUSE OF #material#"],
+"material": ["SAND", "DUST", "LEAVES", "PAPER", "TIN", "ROOTS", "BRICK", "BROKEN DISHES", "WOOD", "STRAW", "WEEDS"],
+
+});
+
 function setup() {
   createCanvas(800, 800);
+  frameRate(1);
 }
 
 function draw() {
   background(220);
+  poem = grammar.flatten("#origin#");
+  textFont("Impact");
+  textSize(50);
+  text(poem,70,200);
+
 }
