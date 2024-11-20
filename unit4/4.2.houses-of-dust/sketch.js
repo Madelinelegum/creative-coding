cee326148337d85685b6505b5e23c18f917eacb0
Madelinelegum <maddierlegum@gmail.com>
2024-11-20 11:45:30 -0500

    update

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,21 +1,35 @@
+//My Poem was inspired by Alison Knowles's "House of Dust." It is about urban dynamics, highlighting the diversity and complexity of city life. Each term I have chosen represents a new aspect of these city environments.
+
 let poem = '';
 
+// Adding the different elements
 let grammar = tracery.createGrammar({
-"origin": ["A HOUSE OF #material#"],
-"material": ["SAND", "DUST", "LEAVES", "PAPER", "TIN", "ROOTS", "BRICK", "BROKEN DISHES", "WOOD", "STRAW", "WEEDS"],
-
+"origin": ["A CITY OF #material#\n#place#\nUSING #light_source#\nINHABITED BY #inhabitants#"], 
+"material": ["GLASS", "ASPHALT", "BRICK", "STEEL", "PLASTIC", "STONE", "COPPER", "PORCELAIN", "FIBERGLASS", "ALUMINUM", "CERAMIC"],
+"place": ["IN A HOUSE", " IN A CONDO", "IN AN APARTMENT", "IN A FIRE STATION", "IN A HOSPITAL", "IN A SCHOOL", "IN A STORE", "IN A SUPERMARKET", "IN A RESTAURANT", "IN A TRAIN STATION", "IN A PARK"],
+"light_source": ["STREET LIGHTS", "SPOTLIGHTS", "CAR LIGHTS", "NEON SIGNS", "TRAFFIC LIGHTS", "FLOODLIGHTS", "STREET LIGHTS", "PARK LIGHTS", "FESTIVE LIGHTS", "EMERGENCY LIGHTS", "BILLBOARDS"],
+"inhabitants": ["STUDENTS", "BIRDS", "FAMILIES", "TOURISTS", "CHILDREN", "ARTISTS", "ANIMALS"],
 });
 
 function setup() {
-  createCanvas(800, 800);
-  frameRate(1);
+  createCanvas(800, 800); // canvas size
+  noLoop(); // disabling draw loop
+  frameRate(0.5); // frame rate
 }
 
 function draw() {
-  background(220);
-  poem = grammar.flatten("#origin#");
-  textFont("Impact");
-  textSize(50);
-  text(poem,70,200);
+  background(220); // background color 
+  textFont("Courier New"); // font style
+  textSize(35); //size of text
+  let x = 10; // initial x positon for the text 
+  let y = 50; // initial y positon for the text 
 
+  for (let i = 0; i < 5; i++) { // loop for multiple quatrains 
+    let lines = grammar.flatten("#origin#").split("\n"); // splitting the grammar - refernce for split https://www.w3schools.com/jsref/jsref_split.asp
+    lines.forEach((line, index) => {
+      text(line, x + index * 70, y + index * 60, width - 40, height - 40); // creating the straggered line position inspired by the orginial poem
+    });
+    y += 260; // the y position for the next quatrain
+
+  }
 }
