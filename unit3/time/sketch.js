e04577df8002402d5ae692d564329902bfa3429b
Madelinelegum <maddierlegum@gmail.com>
2024-12-09 20:06:40 -0500

    update

--- unit3/time/sketch.js
+++ unit3/time/sketch.js
@@ -1,13 +1,17 @@
+// I chose to do the night sky because when I think of time passing by, I think of the nights when my brother and I would lay outside in the grass and watch the clouds move in the night sky. I remember we would lie there for hours until we were told to come back inside, but we would watch how far the clouds would move when we observed them. I don't know how much time we spent outside watching during some periods, but it would sometimes be for hours.
+
+//time related x-coordinate variables
 let x_hour = 50;
 let x_minute = 50;
 let x_second = 50;
 let x_mil = 50;
 
+//varibales for tracking time changes
 let currentSecond;
 let milliFreeze = 0;
 let mm;
 
-let stars = []; // star position
+let stars = []; //stores the star positions as vectors inside the list, to randomize their postion on x and y
 
 function setup() {
   createCanvas(600, 400);
@@ -16,33 +20,20 @@ function setup() {
   // draw stars in the background , https://editor.p5js.org/jesse_harding/sketches/0szF7gcAx
   for (let i = 0; i < 100; i++) {
     stars.push(createVector(random(width), random(height / 2), random
-    (1,4))); // upper half has randomized stars
+    (1,6))); // upper half has randomized stars
   }
 }
 
 function draw() {
-
   background(0,0, 128); // blue background
   
-
-
 // drawing stars
 for(let i = 0; i < stars.length; i++) {
   stroke(255, 255, 150);
   point(stars[i].x, stars[i].y);
 }
-  // textSize(25);
-  // text("hour: " + hour(), 50, 50);
-  // text("minute: " + minute(), 50, 75);
-  // text("second: " + second(), 50, 100);
-  // text("millisecond: " + mm, 50, 125); 
 
-
-  // text("Day: " + day(), 50, 110);
-  // text("Month: " + month(), 50, 125);
-  // text("Year: " + year(),50, 140);
-
-  // Check if we have a new second
+  // Second change and update millsecond counter
   if (currentSecond != second()){
     currentSecond = second();
     milliFreeze = millis();
@@ -65,21 +56,17 @@ for(let i = 0; i < stars.length; i++) {
     fill(255,255,255); // cloud color
 
 
-  //assign value to x_hour using map()
-    x_hour = map(hour(), 0, 23, 50, 550);
+  //The map time values to x-coordinates
+    x_hour = map(hour(), 0, 23, 50, 550); 
     drawCloud(x_hour, 150, 60, 40);
 
-
-  // assign value to x_minute using map()
     x_minute = map(minute(), 0, 59, 50, 550);
     drawCloud(x_minute, 200, 50, 30);
 
-  // assign value to x_second using map()
-  x_second = map(second(), 0, 59, 50, 550);
+    x_second = map(second(), 0, 59, 50, 550);
     drawCloud(x_second, 250, 40, 20);
 
-  //assign value to x_mil using map()
-  x_mil = map(mm, 0, 999, 50, 550);
+    x_mil = map(mm, 0, 999, 50, 550);
     drawCloud(x_mil, 300, 30, 15);
   
     pop();
@@ -91,27 +78,12 @@ function drawCloud(x, y, w, h) {
   
   //main cloud shape with three humps
   ellipse(x, y, w, h); // center hump
-  // ellipse(x - w * 0, y, w * 0, h); //left hump
-  // ellipse(x + w * 0, y, w * 0, h); //right hump
-
-  //adding smaller ellipses to make cloud more fluffy
-
   ellipse(x - w * 0.5, y + h * 0.1, w * 0.7, h * 0.7); //left hump
   ellipse(x + w * 0.5, y + h * 0.1, w * 0.7, h * 0.7); //right hump
-  // ellipse(x, y + h * 0.2, w * 0.6, h * 0.6);
+
 }
 
-  //cloud shape using ellipse
 
-
-  // ellipse(x, y, w, h); //main cloud body
-  // ellipse(x + 20, y - 0, w * 0.7, h * 0.7);
-  // ellipse(x - 20, y - 0, w * 0.7, h * 0.7);
-  // ellipse(x + 45, y - 0, w * 0.5, h * 0.5);
-  // ellipse(x - 45, y - 0, w * 0.5, h * 0.5);
-  // ellipse(x + 40, y + 0, w * 0.9, h * 0.9);
-  // ellipse(x - 40, y + 0, w * 0.9, h * 0.9);
-  
 
 
 
